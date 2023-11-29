be474ebcbd8e9bb2e5a07fed1784c218d8a61a7c
Paco Bernabe <paco.bernabe@shell.com>
2023-11-29 23:54:55 +0100

    copying also owner and group

--- sa-container-redesign/move-acls.sh
+++ sa-container-redesign/move-acls.sh
@@ -21,15 +21,29 @@ blob_containers=("curated" "enriched-harmonized" "enriched-unharmonized" "raw")
 # Get Storage Account Key
 account_key=$(az storage account keys list --resource-group $resource_group_name --account-name $storage_account_name --output json | jq -r '.[0].value')
 
-# 1. Get ACLs of the list of blob containers
+# Loop through the list of containers
 for container in "${blob_containers[@]}"; do
-    acl=$(az storage fs access show --account-name $storage_account_name --path / --file-system $container --account-key $account_key --output json | grep acl | awk '{print $2}' | sed -e "s/\"//g" | sed -e "s/\,$//g")
+    
+    # Does the container exist?
+    exists=$(az storage fs exists --name $container --account-name $storage_account_name --account-key $account_key | grep exists | awk '{print $2}')
 
-    # 2. Make directories with the same name inside the existing blob container
-    az storage fs directory create --account-name $storage_account_name --account-key $account_key --file-system $existing_container --name $container
-    az storage fs access update-recursive --acl $acl --path /$container --file-system $existing_container --account-name $storage_account_name --account-key $account_key
+    # Performing the operations if it does exist
+    if [ $exists = "true" ];
+    then
+        # Get ACLs, owners and groups of the list of blob containers
+        acl=$(az storage fs access show --account-name $storage_account_name --account-key $account_key --file-system $container --path / --output json | grep acl | awk '{print $2}' | sed -e "s/\"//g" | sed -e "s/\,$//g")
+        owner=$(az storage fs access show --account-name $storage_account_name --account-key $account_key --file-system $container --path / --output json | grep owner | awk '{print $2}' | sed -e "s/\"//g" | sed -e "s/\,$//g")
+        group=$(az storage fs access show --account-name $storage_account_name --account-key $account_key --file-system $container --path / --output json | grep \"group\" | awk '{print $2}' | sed -e "s/\"//g" | sed -e "s/\,$//g")
 
-    echo "Directory '$container' created with ACL in '$existing_container'."
+        # Make directories with the same name inside the existing blob container
+        az storage fs directory create --account-name $storage_account_name --account-key $account_key --file-system $existing_container --name $container
+ 
+        # Set ACLs in the new directories (including owner and group)
+        az storage fs access set --account-name $storage_account_name --account-key $account_key --acl $acl --file-system $existing_container --path /$container --owner $owner --group $group
+        az storage fs access update-recursive --account-name $storage_account_name --account-key $account_key --acl $acl --file-system $existing_container --path /$container
+
+        echo "Directory '$container' created with ACL in '$existing_container'."
+    else
+        echo "WARNING: The container '$container' doesn't exist."
+    fi
 done
-
-echo "Script executed successfully."
