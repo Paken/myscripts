77a54d2869fb2cf03512cc1ef64aee458699df42
Paco Bernabe <paco.bernabe@shell.com>
2023-11-29 14:33:11 +0100

    changed the storage subscommands

--- sa-container-redesign/move-acls.sh
+++ sa-container-redesign/move-acls.sh
@@ -16,18 +16,19 @@ resource_group_name=$2
 existing_container="deltalake"
 
 # List of Blob Containers
-blob_containers=("curated", "enriched-harmonized", "enrich-unharmonized", "raw")
+blob_containers=("curated" "enriched-harmonized" "enriched-unharmonized" "raw")
 
 # Get Storage Account Key
 account_key=$(az storage account keys list --resource-group $resource_group_name --account-name $storage_account_name --output json | jq -r '.[0].value')
 
 # 1. Get ACLs of the list of blob containers
 for container in "${blob_containers[@]}"; do
-    acl=$(az storage container show-permission --account-name $storage_account_name --account-key $account_key --name $container --auth-mode key --output json)
-    
+    acl=$(az storage fs access show --account-name $storage_account_name --path / --file-system $container --account-key $account_key --output json | grep acl | awk '{print $2}' | sed -e "s/\"//g" | sed -e "s/\,$//g")
+
     # 2. Make directories with the same name inside the existing blob container
-    az storage blob upload --account-name $storage_account_name --account-key $account_key --container-name $existing_container --name $container/acl.json --type block --content-type "application/json" --content-encoding "utf-8" --content $acl
-    
+    az storage fs directory create --account-name $storage_account_name --account-key $account_key --file-system $existing_container --name $container
+    az storage fs access update-recursive --acl $acl --path /$container --file-system $existing_container --account-name $storage_account_name --account-key $account_key
+
     echo "Directory '$container' created with ACL in '$existing_container'."
 done
 
